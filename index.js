23413d901759860693e5c165aa2e305128bd4917
Sebastian Waisbrot <seppo0010@gmail.com>
2022-05-30 00:45:25 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import { execa } from 'execa';
+import * as fs from 'fs';
+import { parseSync } from 'subtitle'
+
+const directory = (process.env.DATAMAKER_DIR || 'data').replace(/\/+$/, '') + '/';
+
+const getSubtitleForFile = async (path) => {
+  const proc = await execa('ffmpeg', ['-i', path, '-map', '0:s:0', '-f', 'srt', '-']);
+  return parseSync(proc.stdout);
+};
+
+(async () => {
+  const files = fs.readdirSync(directory);
+  files.forEach(async (file) => {
+    const subs = await getSubtitleForFile(directory + file);
+    subs.forEach((sub) => {
+      console.log(sub);
+    });
+  });
+})();
