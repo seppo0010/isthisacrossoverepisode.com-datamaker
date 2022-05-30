35ca1abc39347df8dd9da101a598d23c0a6cb530
Sebastian Waisbrot <seppo0010@gmail.com>
2022-05-30 08:38:46 -0300

    Group still images by episode

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import * as fs from 'fs'
 import { parseSync } from 'subtitle'
 import MiniSearch from 'minisearch'
 import striptags from 'striptags'
+import episodeParser from 'episode-parser';
 
 const sourceDirectory = (process.env.DATAMAKER_SRC_DIR || 'data').replace(/\/+$/, '') + '/'
 const targetDirectory = (process.env.DATAMAKER_TARGET_DIR || 'out').replace(/\/+$/, '') + '/'
@@ -21,18 +22,24 @@ const saveStillImage = async (path, start, target) => {
   const files = fs.readdirSync(sourceDirectory)
   files.forEach(async (file) => {
     const path = sourceDirectory + file
+    const episode = episodeParser(file)
+    const episodePath = `${targetDirectory}${episode.season}x${(episode.episode + '').padStart(2, '0')}/`
+    fs.mkdirSync(episodePath, { recursive: true })
     const subs = (await getSubtitleForFile(path)).filter((sub) => sub && sub.data && sub.data.start !== undefined)
 
-    const miniSearch = new MiniSearch({ fields: ['text'], storeFields: ['html'] })
+    const miniSearch = new MiniSearch({ fields: ['text'], storeFields: ['html', 'season', 'episode', 'stillPath'] })
     miniSearch.addAll(subs.map((sub) => ({
       text: striptags(sub.data.text),
       html: sub.data.text,
+      season: episode.season,
+      episode: episode.episode,
+      stillPath: `${episodePath.substr(targetDirectory.length)}${sub.data.start}.png`,
       id: sub.data.start
     })))
-    fs.writeFileSync('out/index.json', JSON.stringify(miniSearch))
+    fs.writeFileSync(targetDirectory + 'index.json', JSON.stringify(miniSearch))
 
     for (const sub of subs) {
-      const target = targetDirectory + sub.data.start + '.png'
+      const target = `${episodePath}${sub.data.start}.png`
       if (fs.existsSync(target)) continue
       await saveStillImage(path, sub.data.start, target)
       process.stderr.write(`created still image for second ${sub.data.start}\n`)
