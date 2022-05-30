f76d8ab30f30aa50d3661be9ae723837cd2a04bc
Sebastian Waisbrot <seppo0010@gmail.com>
2022-05-30 00:49:11 -0300

    Add eslint

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-import { execa } from 'execa';
-import * as fs from 'fs';
+import { execa } from 'execa'
+import * as fs from 'fs'
 import { parseSync } from 'subtitle'
 
-const directory = (process.env.DATAMAKER_DIR || 'data').replace(/\/+$/, '') + '/';
+const directory = (process.env.DATAMAKER_DIR || 'data').replace(/\/+$/, '') + '/'
 
 const getSubtitleForFile = async (path) => {
-  const proc = await execa('ffmpeg', ['-i', path, '-map', '0:s:0', '-f', 'srt', '-']);
-  return parseSync(proc.stdout);
+  const proc = await execa('ffmpeg', ['-i', path, '-map', '0:s:0', '-f', 'srt', '-'])
+  return parseSync(proc.stdout)
 };
 
 (async () => {
-  const files = fs.readdirSync(directory);
+  const files = fs.readdirSync(directory)
   files.forEach(async (file) => {
-    const subs = await getSubtitleForFile(directory + file);
+    const subs = await getSubtitleForFile(directory + file)
     subs.forEach((sub) => {
-      console.log(sub);
-    });
-  });
-})();
+      console.log(sub)
+    })
+  })
+})()
