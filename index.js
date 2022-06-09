1a615136ac785b1046a277c764ec16c45fb5ffce
Sebastian Waisbrot <seppo0010@gmail.com>
2022-06-08 23:33:31 -0300

    Use opensubtitle to fetch data

--- index.js
+++ index.js
@@ -6,21 +6,83 @@ import MiniSearch from 'minisearch'
 import striptags from 'striptags'
 import episodeParser from 'episode-parser';
 import walk from 'walk'
+import OS from 'opensubtitles.com';
+import OS_API from 'opensubtitles-api';
+import fetch from 'node-fetch';
 
 const sourceDirectory = (process.env.DATAMAKER_SRC_DIR || 'data').replace(/\/+$/, '') + '/'
 const targetDirectory = (process.env.DATAMAKER_TARGET_DIR || 'out').replace(/\/+$/, '') + '/'
+const imageExtension = process.env.IMAGE_EXTENSION || 'png'
 const stillWidth = 720
 const thumbnailWidth = 180
 fs.mkdirSync(targetDirectory, { recursive: true })
 
-const getSubtitleForFile = async (path) => {
+const getSubtitleFromSrtFile = async (path) => {
   const srtPath = path.substring(0, path.length - 3) + 'srt'
   if (fs.existsSync(srtPath)) {
     return parseSync(fs.readFileSync(srtPath, 'utf-8'))
   }
-  process.stderr.write(`Extracting subtitles ${path}\n`)
-  const proc = await execa('ffmpeg', ['-i', path, '-map', '0:s:0', '-f', 'srt', '-'])
-  return parseSync(proc.stdout)
+}
+
+const getSubtitleFromTrack = async (path) => {
+  try {
+    const proc = await execa('ffmpeg', ['-i', path, '-map', '0:s:0', '-f', 'srt', '-'])
+    return parseSync(proc.stdout)
+  } catch (e) {
+    // this is probably ok, most videos do not have a subtitle track
+    console.warn('failed to fetch subtitle for', path);
+  }
+}
+
+let os;
+let osLoggedIn = false;
+let OpenSubtitles;
+if (process.env.OSDB_API_KEY) {
+  os = new OS({apikey: process.env.OSDB_API_KEY});
+   OpenSubtitles = new OS_API({
+      useragent: process.env.OSDB_USERAGENT,
+      username:  process.env.OSDB_USERNAME,
+      password:  process.env.OSDB_PASSWORD,
+      ssl: true,
+  })
+}
+
+const getSubtitleFromOpenSubtitle = async (path, episode) => {
+  if (!process.env.OSDB_API_KEY || !process.env.OSDB_USERNAME || !process.env.OSDB_PASSWORD) return
+  if (!osLoggedIn) {
+    await os.login({
+      username: process.env.OSDB_USERNAME,
+      password: process.env.OSDB_PASSWORD,
+    });
+    osLoggedIn = true;
+  }
+  const { moviehash } = await OpenSubtitles.hash(path);
+  try {
+    const { data } = await os.subtitles({
+      moviehash: moviehash,
+      query: process.env.OSDB_QUERY,
+      season_number: episode.season,
+      episode_number: episode.episode,
+      // sending `languages` here should work but it throws an error
+    })
+    const sub = data.filter((row) => row.attributes.language === process.env.OSDB_LANGUAGE)[0]
+    if (!sub) return;
+    const { link } = await os.download({
+      file_id: sub.attributes.files[0].file_id,
+    });
+    const response = await fetch(link);
+    const text = await response.text();
+    // This is a bad side-effect... maybe it should be a configuration...
+    fs.writeFileSync(path.substring(0, path.length - 3) + 'srt', text)
+
+    return parseSync(text)
+  } catch (e) {
+    console.error('failed to fetch subtitle for', path, e)
+  }
+}
+
+const getSubtitleForFile = async (path, episode) => {
+  return getSubtitleFromSrtFile(path) || getSubtitleFromTrack(path) || getSubtitleFromOpenSubtitle(path, episode);
 }
 
 const saveStillImage = async (path, start, target, width) => {
@@ -40,7 +102,8 @@ const saveStillImage = async (path, start, target, width) => {
     }
     const episodePath = `${targetDirectory}${episode.season}x${(episode.episode + '').padStart(2, '0')}/`
     fs.mkdirSync(episodePath, { recursive: true })
-    const subs = (await getSubtitleForFile(filePath)).filter((sub) => sub && sub.data && sub.data.start !== undefined)
+    const subs = (await getSubtitleForFile(filePath, episode) || [])
+      .filter((sub) => sub && sub.data && sub.data.start !== undefined)
 
     process.stderr.write('adding subtitles to index\n')
     // minisearch configuration must match web's
@@ -53,12 +116,12 @@ const saveStillImage = async (path, start, target, width) => {
     })))
 
     for (const sub of subs) {
-      const target = `${episodePath}${sub.data.start}_still.png`
+      const target = `${episodePath}${sub.data.start}_still.${imageExtension}`
       if (!fs.existsSync(target)) {
         process.stderr.write('creating still image\n')
         await saveStillImage(filePath, sub.data.start, target, stillWidth)
       }
-      const thumbnail = `${episodePath}${sub.data.start}_thumbnail.png`
+      const thumbnail = `${episodePath}${sub.data.start}_thumbnail.${imageExtension}`
       if (!fs.existsSync(thumbnail)) {
         process.stderr.write('creating thumbnail image\n')
         await saveStillImage(filePath, sub.data.start, thumbnail, thumbnailWidth)
